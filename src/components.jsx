f28ce5e1e114f6653b581211b22d93c3b9e1e6a8
Praveen kumar <vemulapraveenkumar705@gmail.com>
2024-12-30 11:32:37 +0530

    new

--- src/components.jsx
+++ src/components.jsx
@@ -2,7 +2,7 @@
 function Components() {
     return(
         <>
-        <div className="bg-primary text-center p-3 mt-5">chandu naidu</div>
+        <div className="bg-primary text-center mt-5">chandu</div>
         </>
     )
 }
