78ab1cc961f5ab45408d2bb9bc8e745368c2aee3
Praveen kumar <vemulapraveenkumar705@gmail.com>
2024-12-30 11:27:41 +0530

    second-test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
+import Components from './components';
 function App() {
   return (
+    <>
     <div className='text-center'>Praveen Naidu</div>
+    <Components/>
+    </>
   );
 }
 
---
+++ src/components.jsx
@@ -0,0 +1,10 @@
+
+function Components() {
+    return(
+        <>
+        <div className="bg-primary text-center p-3 mt-5">chandu naidu</div>
+        </>
+    )
+}
+
+export default Components;
